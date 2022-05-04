0f6b6377c5ff1302c9bfb96c758eddcb794cc0e7
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-05-04 15:40:48 +0200

    creato file .php con dentro la struttura dati e incluso questo file nel .php che esegue le operazioni su tale struttura dati

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+//phpinfo();
+
+//importo il file contenente la struttura dati
+include('strutturaDati.php');
+/*
+foreach($questions as $question)
+{
+    ech
+    echo('<h2>');
+
+}*/
+
+?>
