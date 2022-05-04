40cfb1cbe67d25127a9c4a7630946a107b7e20d3
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-05-04 16:04:48 +0200

    aggiunto file .css esterno e usato il php per creare il tag head con dentro il tag link da mettere dentro l'html generato

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 
 //importo il file contenente la struttura dati
 include('strutturaDati.php');
+include('style.css');
 
 foreach($topics as $topic)
 {
+    echo(' <head>
+                <link rel="stylesheet" href="style.css">
+           </head> ');
     echo('<div>');
     echo('<h2>');
     echo($topic["question"]);
